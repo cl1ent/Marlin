d8665a6209c1edd8511b011d5731e73bb3309460
cl1ent <cl1ent@bk.ru>
2022-04-09 14:23:12 +0200

    added custom hotend pid tuning parameters

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -601,9 +601,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  28.43
+    #define DEFAULT_Ki   2.34
+    #define DEFAULT_Kd  86.43
   #endif
 #endif // PIDTEMP
 
